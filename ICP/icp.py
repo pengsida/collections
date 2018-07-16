bf098b18905d57bf8546901504b8f0a0e20a14f5
pengsida <pengsida@zju.edu.cn>
2018-07-16 13:26:10 +0800

    update reference

--- ICP/icp.py
+++ ICP/icp.py
@@ -1,3 +1,5 @@
+# reference: http://ais.informatik.uni-freiburg.de/teaching/ss11/robotics/slides/17-icp.pdf
+# reference: https://github.com/ClayFlannigan/icp/blob/master/icp.py
 # reference: https://gist.github.com/ecward/c373932638fd04a2243e
 
 class ICP(object):
