375e514ac3b63badb6fa601afcf8f36823ebf398
pengsida <pengsida@zju.edu.cn>
2018-07-21 23:38:38 +0800

    split a node

--- RandomForest/random_forest.py
+++ RandomForest/random_forest.py
@@ -15,25 +15,70 @@ class RandomForest(object):
         self.N, self.M = dataset.shape[0], dataset.shape[1] - 1
         self.nr_feat = int(np.sqrt(self.N))
 
+    def gini_index(self, dataset, split):
+        """
+        Algorithm:
+          1. count the total number of features nr_feat
+          2. split the dataset to left and right according to the split
+          3. the gini_index of left: (1 - p_1^2 - p_2^2) / nr_left_feat * nr_feat
+          4. compute the gini_index of right likewise
+        """
+        nr_feat = dataset.shape[0]
+        left, right = dataset[split, -1], dataset[~split, -1]
+        nr_left_feat, nr_right_feat = len(left), len(right)
+
+        def div_cls(cls, p, nr):
+            if len(cls) == 0:
+                return 0, 0
+            if len(cls) == 2:
+                return p / nr
+            return 0, p[0] / nr
+
+        gini_index = 0
+        if len(left) != 0:
+            cls, p = np.unique(left, return_counts=True)
+            p1, p2 = div_cls(cls, p, nr_left_feat)
+            gini_index += (1 - p1**2 - p2**2) / nr_left_feat * nr_feat
+
+        if len(right) != 0:
+            cls, p = np.unique(right, return_counts=True)
+            p1, p2 = div_cls(cls, p, nr_right_feat)
+            gini_index += (1 - p1**2 - p2**2) / nr_right_feat * nr_feat
+
+        return gini_index
+
     def split_dataset(self, dataset, index):
         """
         Algorithm:
           1. compute the matrix that splits the dataset to left and right subtree according to the feature value
           2. split the dataset based on the matrix
-          3. choose the feature value maximizing the gini index
+          3. choose the feature value minimizing the gini index
         """
         min_gini = None
+        min_idx = 0
         feat_val = dataset[:, index]
-        left_or_right = (feat_val[:, np.newaxis] <= feat_val[np.newaxis, :]).astype(np.int32)
-        for split in left_or_right:
-            left, right = dataset[split], dataset[~split]
-
+        left_or_right = (feat_val[:, np.newaxis] >= feat_val[np.newaxis, :])
+        for idx, split in enumerate(left_or_right):
+            gini = self.gini_index(dataset, split)
+            min_gini, min_idx = (gini, idx) if min_gini is None or gini < min_gini else (min_gini, min_idx)
+        return min_gini, min_idx
 
     def get_split_point(self, dataset):
+        """
+        Algorithm:
+          1. select the decision features randomly
+          2. select the feature minimizing the gini index
+        """
         feat_index = np.random.choice(self.M, self.nr_feat, replace=False)
-        import pdb; pdb.set_trace()
+        min_rs = None
+        min_idx = 0
         for index in feat_index:
-            left, right = self.split_dataset(dataset, index)
+            rs = self.split_dataset(dataset, index)
+            min_rs, min_idx = (rs, index) if min_rs is None or rs[0] < min_rs[0] else (min_rs, min_idx)
+        pivot = dataset[min_rs[1], min_idx]
+        split = (pivot >= dataset[:, min_idx])
+        left, right = dataset[split], dataset[~split]
+        return {'gini': min_rs[0], 'idx': min_idx, 'left': left, 'right': right}
 
     def train(self):
         self.get_split_point(dataset)
