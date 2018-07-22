1a52f5c2f39515642e46db20a86f45e31dba4f5b
pengsida <pengsida@zju.edu.cn>
2018-07-22 14:35:17 +0800

    implement random forest

--- RandomForest/random_forest.py
+++ RandomForest/random_forest.py
@@ -2,6 +2,7 @@
 # reference: https://github.com/amstuta/random-forest/tree/master/python
 import numpy as np
 import pandas as pd
+from multiprocessing.dummy import Pool
 
 
 def read_dataset(filename):
@@ -9,23 +10,30 @@ def read_dataset(filename):
     return data
 
 
-class RandomForest(object):
-    def __init__(self, dataset, max_depth):
+class DecisionNode(object):
+    def __init__(self, pivot=None, idx=None, left=None, right=None, cls=None):
+        self.pivot = pivot
+        self.idx = idx
+        self.left = left
+        self.right = right
+        self.cls = cls
+
+
+class DecisionTree(object):
+    def __init__(self, max_depth=-1):
         self.max_depth = max_depth
-        self.N, self.M = dataset.shape[0], dataset.shape[1] - 1
-        self.nr_feat = int(np.sqrt(self.N))
+        self.tree = None
 
     def gini_index(self, dataset, split):
         """
         Algorithm:
           1. count the total number of features nr_feat
           2. split the dataset to left and right according to the split
-          3. the gini_index of left: (1 - p_1^2 - p_2^2) / nr_left_feat * nr_feat
-          4. compute the gini_index of right likewise
+          3. compute the gini index: p * creterion(left) + (1 - p) * creterion(right)
         """
         nr_feat = dataset.shape[0]
         left, right = dataset[split, -1], dataset[~split, -1]
-        nr_left_feat, nr_right_feat = len(left), len(right)
+        p = len(left) / nr_feat
 
         def div_cls(cls, p, nr):
             if len(cls) == 0:
@@ -34,16 +42,14 @@ class RandomForest(object):
                 return p / nr
             return 0, p[0] / nr
 
-        gini_index = 0
-        if len(left) != 0:
-            cls, p = np.unique(left, return_counts=True)
-            p1, p2 = div_cls(cls, p, nr_left_feat)
-            gini_index += (1 - p1**2 - p2**2) / nr_left_feat * nr_feat
+        def creterion(data):
+            if len(data) == 0:
+                return 0
+            cls, p = np.unique(data, return_counts=True)
+            p1, p2 = div_cls(cls, p, len(data))
+            return 1 - p1**2 - p2**2
 
-        if len(right) != 0:
-            cls, p = np.unique(right, return_counts=True)
-            p1, p2 = div_cls(cls, p, nr_right_feat)
-            gini_index += (1 - p1**2 - p2**2) / nr_right_feat * nr_feat
+        gini_index = p * creterion(left) + (1 - p) * creterion(right)
 
         return gini_index
 
@@ -78,14 +84,81 @@ class RandomForest(object):
         pivot = dataset[min_rs[1], min_idx]
         split = (pivot >= dataset[:, min_idx])
         left, right = dataset[split], dataset[~split]
-        return {'gini': min_rs[0], 'idx': min_idx, 'left': left, 'right': right}
+        return DecisionNode(pivot, min_idx, left, right)
 
-    def train(self):
-        self.get_split_point(dataset)
+    def build_node(self, dataset):
+        cls, counts = np.unique(dataset[:, -1], return_counts=True)
+        inds = np.argsort(counts)
+        return DecisionNode(cls=cls[inds[-1]])
+
+    def build_tree(self, dataset, depth):
+        if depth == 0:
+            return self.build_node(dataset)
+
+        node = self.get_split_point(dataset)
+        if len(node.left) == 0 or len(node.right) == 0:
+            node = self.build_node(dataset)
+        else:
+            node.left = self.build_tree(node.left, depth - 1)
+            node.right = self.build_tree(node.right, depth - 1)
+        return node
+
+    def fit(self, dataset):
+        self.N, self.M = dataset.shape[0], dataset.shape[1] - 1
+        self.nr_feat = int(np.sqrt(self.N))
+        self.tree = self.build_tree(dataset, depth=self.max_depth)
+
+    def predict(self, feat):
+        cur_node = self.tree
+        while cur_node.cls is None:
+            pivot = cur_node.pivot
+            idx = cur_node.idx
+            if feat[idx] <= pivot:
+                cur_node = cur_node.left
+            else:
+                cur_node = cur_node.right
+        return cur_node.cls
+
+
+class RandomForest(object):
+    def __init__(self, nr_trees, max_depth=-1):
+        self.nr_trees = nr_trees
+        self.max_depth = max_depth
+        self.trees = None
+
+    def build_tree(self, dataset):
+        tree = DecisionTree(self.max_depth)
+        tree.fit(dataset)
+        return tree
+
+    def fit(self, dataset):
+        nr_samples = len(dataset) // 10 * 9
+        with Pool(self.nr_trees) as pool:
+            data = map(lambda i: np.random.choice(len(dataset), size=nr_samples), range(self.nr_trees))
+            data = map(lambda i: dataset[i], data)
+            self.trees = pool.map(self.build_tree, data)
+
+    def predict(self, feat):
+        preds = [tree.predict(feat) for tree in self.trees]
+        cls, counts = np.unique(preds, return_counts=True)
+        inds = np.argsort(counts)
+        return cls[inds[-1]]
+
+
+def demo():
+    dataset = read_dataset('./sonar.all-data')
+    size = len(dataset)
+    train_size = size // 5 * 4
+    train, test = dataset[:train_size], dataset[train_size:]
+    model = RandomForest(5, 8)
+    model.fit(train)
+
+    rec = 0
+    for feat in test:
+        rec += int(feat[-1] == model.predict(feat))
+    print('accuracy: {}'.format(rec / len(test)))
 
 
 if __name__ == '__main__':
-    dataset = read_dataset('./sonar.all-data')
-    model = RandomForest(dataset, 5)
-    model.train()
+    demo()
 
