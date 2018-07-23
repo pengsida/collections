9532d301d5259740deeab9fc380428ca5defaedf
pengsida <pengsida@zju.edu.cn>
2018-07-23 14:32:38 +0800

    add reference of hough voting

---
+++ HoughVoting/hough_voting.py
@@ -0,0 +1,6 @@
+# reference: http://aishack.in/tutorials/hough-transform-basics/
+# reference: http://aishack.in/tutorials/hough-transform-normal/
+# reference: https://alyssaq.github.io/2014/understanding-hough-transform/
+# reference: https://github.com/alyssaq/hough_transform
+# reference: https://github.com/vmonaco/general-hough/blob/master/src/GeneralHough.py
+
