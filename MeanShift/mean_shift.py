84fb0ae2562c1ee6bfcc1fa801928fb070586e27
pengsida <pengsida@zju.edu.cn>
2018-07-24 17:18:15 +0800

    add Mean Shift

---
+++ MeanShift/mean_shift.py
@@ -0,0 +1,5 @@
+# reference: http://vision.stanford.edu/teaching/cs131_fall1314_nope/lectures/lecture13_kmeans_cs131.pdf
+# reference: http://www.cnblogs.com/liqizhou/archive/2012/05/12/2497220.html
+# reference: https://spin.atomicobject.com/2015/05/26/mean-shift-clustering/
+# reference: https://github.com/mattnedrich/MeanShift_py
+
